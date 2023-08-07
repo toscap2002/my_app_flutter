0436306fd38b0b175ad339893c4c613e95cc4e66
tosca <s1098079@studenti.univpm.it>
2023-08-07 09:37:55 +0200

    Bottom navigation bar

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +15,10 @@ class HomePage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: Colors.grey.shade800,
       appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.pinkAccent,
         actions: [
           IconButton(
               onPressed: logoutUser,
@@ -25,7 +29,25 @@ class HomePage extends StatelessWidget{
       body: Center(child: Text("Sei entrato come " + user.email!,
       style: TextStyle(fontSize: 20),
       ),
-      )
+      ),
+        bottomNavigationBar: CurvedNavigationBar(
+          backgroundColor: Colors.grey.shade800,
+          color: Colors.pinkAccent,
+          animationDuration: Duration(milliseconds: 300),
+          onTap: (index){
+            print(index);
+          },
+          items: [
+            Icon(
+                Icons.search,
+                color: Colors.white,
+            ),
+            Icon(
+                Icons.local_fire_department_sharp,
+                color: Colors.white,
+            ),
+      ],
+    ),
     );
   }
 }
