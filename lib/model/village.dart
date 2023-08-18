a4867204674526de19e2226c37aafa88a8dd8c8a
tosca <s1098079@studenti.univpm.it>
2023-08-18 14:19:18 +0200

    esempio api

---
+++ lib/model/village.dart
@@ -0,0 +1,8 @@
+class Village{ //sono collegati ai json array
+  final String jsonObgect1;
+  final String jsonObgect2;
+
+  Village  ({
+    required this.jsonObgect1,
+    required this.jsonObgect2});
+}
--- lib/pages/topPage.dart
+++ lib/pages/topPage.dart
@@ -1,19 +1,67 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:my_app_flutter/model/village.dart';
 
 class TopPage extends StatefulWidget {
-  const TopPage({super.key});
+  TopPage({super.key});
+
 
   @override
   State<TopPage> createState() => _TopPageState();
 }
 
+List<Village> villages = [];
+
+
+Future getTopPlayer() async{
+  var response = await http.get(Uri.https('api'));
+  //print(response.body);
+  var jsonData = jsonDecode(response.body);
+
+  for(var eachPlayer in jsonData['nome array json']){
+    final village = Village(
+        jsonObgect1: eachPlayer['jsonObgect1'],
+        jsonObgect2: eachPlayer['jsonObgect2'],
+    );
+    villages.add(village);
+  }
+  print(villages.length);
+}
+
+
 class _TopPageState extends State<TopPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
+    getTopPlayer();
+    return Scaffold(
       //La top 50
-      //backgroundColor: Colors.grey.shade600,
-      child: Text('La Top 50 !!!!!!'),
+      backgroundColor: Colors.grey.shade600,
+      body: FutureBuilder(
+        future: getTopPlayer(),
+        builder: (context, snapshot){
+          //ha caricato? allora mostra i dati
+          if(snapshot.connectionState == ConnectionState.done){
+            return ListView.builder(
+                itemCount: villages.length,
+                itemBuilder: (context, index){
+                  return ListTile(
+                    title: Text(villages[index].jsonObgect1),
+                    subtitle: Text(villages[index].jsonObgect2),
+                  );
+                },
+            );
+          }
+          //se sta ancora caricando, mostra il cerchio di caricmento
+          else{
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+        },
+      ),
     );
 
 
